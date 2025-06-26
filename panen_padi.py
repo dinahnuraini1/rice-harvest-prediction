a4d25c95ee529bacbabeb4e4935df1b987708fdf
dinahnuraini <112395290+dinahnuraini1@users.noreply.github.com>
2025-06-26 08:37:07 +0700

    Update panen_padi.py

--- panen_padi.py
+++ panen_padi.py
@@ -666,8 +666,7 @@ def main():
     
         # 2. Load model dari Google Drive (sekali saja)
         if "model_rf_pso_best" not in st.session_state:
-            import tempfile, gdown
-    
+
             drive_id = "1LZqDyupjcoY_RO3BFFE7McREHv2A2P01"
             url = f"https://drive.google.com/uc?id={drive_id}"
     
